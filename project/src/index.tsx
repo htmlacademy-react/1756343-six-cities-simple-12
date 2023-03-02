fbc276c5cc147d61b01828a598faaef9915558f3
Alferova Ekaterina <User@Administrators-MacBook-Pro.local>
2023-03-02 23:53:14 +0300

    added new page components

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Data = {
+  placesFound: 776,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App placesFound={Data.placesFound} />
   </React.StrictMode>,
 );
